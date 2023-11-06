6580d2f356658d690b10a1e67fad028fd2a50451
devGiyosiddin <devgiyosiddin@gmail.com>
2023-11-06 16:19:40 +0500

    add close button to mofal and his JS function

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 const elModalOpener = document.querySelector('.modal-opener');
 const elModal = document.getElementById('modal');
+const elModalCloseButton = document.querySelector('.modal-close-button');
 
 elModalOpener.addEventListener('click', function() {
     elModal.classList.add('modal-opener');
-    //close modal on escape;
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'Escape') {
-            elModal.classList.remove('modal-opener')
-        }
-    })
+});
+elModalCloseButton.addEventListener('click', function() {
+    elModal.classList.remove('modal-opener')
 });
